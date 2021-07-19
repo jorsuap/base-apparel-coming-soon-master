02abe19f9c0f42b6dea1ac7ff2a18b1706e1c418
jorsuap <jorsuap@gmail.com>
2021-07-19 11:09:37 -0500

    master

--- js/validar.js
+++ js/validar.js
@@ -1,10 +1,11 @@
 function validarcorreo(correo){
     var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
     var valido = expReg.test(correo);
-    if(valido==true){
-        alert("valido");
+    var stilo = document.getElementById("email");
+    var msjalert = document.getElementById("alert")
+    if(valido==false){
+        stilo.classList.add("error");
+        msjalert.classList.add("active");
     }
-    else{
-        alert("Invalido");
-    }
+    
 }
